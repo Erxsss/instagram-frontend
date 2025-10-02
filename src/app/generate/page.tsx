283c6afeade782dbc0425b35625f5b1d32a845e4
25HP4283 25HP4283 <25HP4283@G301-CC0211.local>
2025-10-02 18:38:29 +0800

    gg

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -123,7 +123,7 @@ const Page = () => {
           <img className="rounded-2xl" src={imageUrl} alt="" />
         </div>
         <div>
-          <Button onClick={createPost}>Create Post</Button>
+          <Button onClick={createPost}>Create Post okay</Button>
         </div>
       </div>
     </div>
